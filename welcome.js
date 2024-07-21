db74270f6a91c8cbe6c17086bee75c6c3ec19053
Zandgar11 <153857693+Zandgar11@users.noreply.github.com>
2024-07-21 17:32:46 +0200

    Update welcome.js

--- welcome.js
+++ welcome.js
@@ -1,1 +1,11 @@
+document.getElementById('name-form').addEventListener('submit', function(event) {
+    event.preventDefault(); // Empêche le rechargement de la page
+    const playerName = document.getElementById('player-name').value;
+    if (playerName) {
+        // Stocker le prénom dans le stockage local
+        localStorage.setItem('playerName', playerName);
+        // Rediriger vers la page du jeu
+        window.location.href = 'game.html';
+    }
+});
 
